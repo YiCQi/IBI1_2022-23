d59ff7bfce6b609f4333ecbc90b571a8b3ea19ab
YiCQi <123134969+YiCQi@users.noreply.github.com>
2023-03-14 10:51:02 +0800

    Update hexagons.py

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,5 +1,5 @@
-for n in range(1,6)
-  h = 2*n*(2n-1)/2
-  print(h)
+for n in range(1,6) # set a loop
+  h = 2*n*(2n-1)/2 # calculate the h
+  print(h) # present the result
   
 # It's too simple. It's hard to comment on it.
