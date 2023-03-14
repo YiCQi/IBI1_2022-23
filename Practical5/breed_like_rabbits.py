a07c5a2cde7bc9d11d344c66d892978ceb497a16
YiCQi <123134969+YiCQi@users.noreply.github.com>
2023-03-14 11:32:36 +0800

    Update breed_like_rabbits.py

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,5 +1,8 @@
-n = 2; g = 0
-while n <= 100:
+n = 2 # the original number of rabbits 
+g = 0 # generation of rabbits
+while n <= 100: # when number is larger than 100, end loop
     g += 1
     n = 2 ** (g+1)
 print(g)
+
+# answer: it takes 6 generations
