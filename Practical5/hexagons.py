9e52ab7382f035e599cf6566e6592f78e6387e8c
YiCQi <123134969+YiCQi@users.noreply.github.com>
2023-03-20 10:44:46 +0800

    Update hexagons.py

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,9 +1,14 @@
 # This code aims to use a formula to calculate the number of all elements in 5 cases.
+# A loop was set to handle 5 cases, n is the sequence number and h is the result in every case.
 
 # Set a loop from 1 to 5
-for n in range(1,6)
+for n in range(1,6):
     # Create formulas using known rules
-    h = 2*n*(2n-1)/2
+    h = 2*n*(2*n-1)/2
     print(h)
   
-# It's too simple. It's hard to comment on it.
+# output:   1.0
+#           6.0
+#           15.0
+#           28.0
+#           45.0
