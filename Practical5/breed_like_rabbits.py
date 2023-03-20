f19deb0a76c482a7bf7d9d356150ed20d9fac84f
YiCQi <123134969+YiCQi@users.noreply.github.com>
2023-03-20 10:53:38 +0800

    Update breed_like_rabbits.py

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,6 +1,6 @@
 # This code aims to calculate the generations taken to multiply the rabbits to the number of 100.
 # The original number of rabbits( n ) is 2 and the original generation( g ) is 1.
-# Every new generation( g += 1 ) the number of rabbits doubles( n = 2 ** g ).
+# Every new generation( g += 1 ) the number of rabbits doubles( n *= 2 ).
 # generation( g ) will be printed as the result.
 
 n = 2 # The original number of rabbits 
@@ -9,7 +9,7 @@ g = 1 # Generation of rabbits
 while n <= 100:
     # every new generation the number of rabbits doubles
     g += 1
-    n = 2 ** g
+    n *= 2
 print(g)
 
 # answer: it takes 7 generations
